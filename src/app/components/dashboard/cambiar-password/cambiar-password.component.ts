ac01c0100ff009e9246cb9cd7b6611fd480355ae
Luis Vanegas <lvanegas1429@gmail.com>
2021-10-14 17:26:08 -0600

    Validaciones personalizada y standart implementada

--- src/app/components/dashboard/cambiar-password/cambiar-password.component.ts
+++ src/app/components/dashboard/cambiar-password/cambiar-password.component.ts
@@ -22,7 +22,7 @@ export class CambiarPasswordComponent implements OnInit {
    }
 
    checkPassword(group: FormGroup): any{
-    const pass = group.controls.passwordAnterior.value;
+    const pass = group.controls.nuevaPassword.value;
     const confirmPass = group.controls.confirmPassword.value;
     
     return pass === confirmPass ? null : {notSame: true};
@@ -31,4 +31,31 @@ export class CambiarPasswordComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  guardarPassword(){
+
+  }
+
+  get passwordAnteriorValido(){
+    return this.cambiarPassword.get('passwordAnterior').valid;
+  }
+  
+  get passwordAnteriorNoValido(){
+    return this.cambiarPassword.get('passwordAnterior').invalid && this.cambiarPassword.get('passwordAnterior').touched;
+  }
+
+  get nuevaPasswordValida(){
+    return this.cambiarPassword.get('nuevaPassword').valid
+  }
+
+  get nuevaPasswordNoValidaLength(){
+    return this.cambiarPassword.get('nuevaPassword').hasError('minlength') && this.cambiarPassword.get('nuevaPassword').touched;
+  }
+
+  get confirmPasswordValido(){
+    return this.cambiarPassword.get('confirmPassword').valid && this.cambiarPassword.get('confirmPassword').touched;
+  }
+
+  get confirmPasswordNoCoinciden(){
+    return this.cambiarPassword.hasError('notSame') && this.cambiarPassword.get('confirmPassword').touched;
+  }
 }
